7adf5dca67175d854652308264e1fdd4f861fde0
Wesley Luo <112664995+r9nyuqi@users.noreply.github.com>
2024-03-26 15:48:42 -0700

    dropspawn

---
+++ Challenge/Assets/dropSpawn.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropSpawn : MonoBehaviour
+{
+    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private GameObject[] dropsPrefabs;
+    [SerializeField] private bool canSpawn = true;
+    private GameObject[] getCount;
+
+    [SerializeField] private Transform top;
+    [SerializeField] private Transform left;
+    [SerializeField] private Transform right;
+    [SerializeField] private Transform bot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Spawner()
+    {
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+        while (true)
+        {
+            //getCount = GameObject.FindGameObjectsWithTag("Enemy");
+            //int count = getCount.Length;
+            //if(count < 1)
+            {
+                yield return wait;
+                int ran = Random.Range(0, dropsPrefabs.Length);
+                GameObject DropsToSpawn = dropsPrefabs[ran];
+                int ranX = (int)Random.Range(left.position.x, right.position.x);
+                int ranY = (int)Random.Range(bot.position.y, top.position.y);
+                Vector2 pos = new Vector2(ranX, ranY);
+                Instantiate(DropsToSpawn, pos, Quaternion.identity);
+            }
+
+        }
+    }
+}
+
